50916210d4cd36bef241ec04180a819ba1278bd4
GrowwStacks2023 <admin@growwstacks.com>
2026-03-05 17:02:29 +0530

    Auto commit 05-03-2026 17:02:29.33

---
+++ auto_push.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd /d C:\wamp64\www\auto-push-testing
+
+git add .
+
+git diff --cached --quiet
+if %errorlevel%==0 (
+    echo No changes to commit
+) else (
+    git commit -m "Auto commit %date% %time%"
+    git push origin main
+    echo Changes pushed to GitHub
+)
+
+exit
