8b9a8ddcf664abc0480d5bea3c90e5e7daabe19c
GrowwStacks2023 <admin@growwstacks.com>
2026-03-05 17:40:49 +0530

    Auto commit 05-03-2026 17:40:49.34

--- auto_push.bat
+++ auto_push.bat
@@ -1,16 +1,18 @@
 @echo off
 
-cd C:\wamp64\www\auto-push-testing
+cd /d C:\wamp64\www\auto-push-testing
+
+echo Checking for changes...
 
 git add .
 
 git diff --cached --quiet
-if %errorlevel%==0 (
+IF %ERRORLEVEL% EQU 0 (
     echo No changes to commit
-) else (
+) ELSE (
     git commit -m "Auto commit %date% %time%"
-    git push origin main
+    git push origin mark
     echo Changes pushed to GitHub
 )
 
-exit
+pause
